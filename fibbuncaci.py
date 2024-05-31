4aae6ba5a7ca5947b8dcf8d6e0c1f81ce64d0467
Huzzi <hussnainshahidd@gmail.com>
2024-05-31 15:20:52 +0500

    commit

--- fibbuncaci.py
+++ fibbuncaci.py
@@ -1,13 +1,13 @@
 print("Fibbunacci Series")
-num=int(input("Enter a number "))
+num=int(input("Enter a Number "))
 
-def fibbunacci(i):
+def Fibbunacci(i):
     if i==0:
         return 0
     elif i==1:
         return 1
     else:
-        return fibbunacci(i-2)+ fibbunacci(i-1)
+        return Fibbunacci(i-2)+ Fibbunacci(i-1)
     
 for i in range(num):
-    print(fibbunacci(i),end=" ")
+    print(Fibbunacci(i),end=" ")
